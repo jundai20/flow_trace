e204ccb276ae1548fa64dc1093a9748d1370d7eb
Jun Dai <jundai20@hotmail.com>
2022-05-26 22:00:04 +0800

    minor change, add test.c

---
+++ test.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define NOP 500000
+
+int baz (void)
+{
+    sleep(1);
+    return random();
+}
+
+int bar (void)
+{
+    sleep(1);
+    return baz();
+}
+
+int main ()
+{
+    int cnt = 0;
+
+    printf("Run: ./spy -p %d -g -o func_info.txt\n", getpid());
+    for (;;) {
+        cnt+= bar();
+    }
+    return cnt;
+}
